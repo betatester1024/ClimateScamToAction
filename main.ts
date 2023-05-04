9baf2a23bff91b107a531b28ad077271c49718a2
John Doe <betatester1025@protonmail.com>
2023-05-04 10:50:52 -0400

    fixed servo being weird

--- main.ts
+++ main.ts
@@ -1,37 +1,43 @@
 input.onButtonPressed(Button.A, function () {
-	
+    cakLandServos.resetServos(cakLandServos.ServoPin.P0)
 })
 let forwardQ = 0
-images.createImage(`
-    # . # . #
-    # . # . #
-    # . # # #
-    # . # . #
-    # . # . #
+images.createBigImage(`
+    . . . . # . . . . .
+    . . . # . . . . . .
+    # . # . . . . . . .
+    . # . . . . . . . .
+    . . . . . . . . . .
     `).showImage(0)
-loops.everyInterval(1000, function () {
+cakLandServos.resetServos(cakLandServos.ServoPin.P0)
+cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.UP)
+cakLandMotor.turnLeft(63)
+basic.forever(function () {
+    cakLandPump.pumpStart(cakLand.BoardSide.RIGHT, 34)
     if (forwardQ == 1) {
         images.createImage(`
-            # # # # #
-            # . . . #
-            # . # . #
-            # . . . #
-            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            . # # # .
+            . . # . .
             `).showImage(0)
-        cakLandServos.turnServo(cakLandServos.ServoPin.P0, 90)
+        cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.MIDDLE)
+        basic.pause(500)
         forwardQ = 0
     } else {
         images.createImage(`
-            . . . . .
+            . . # . .
             . # # # .
-            . # . # .
-            . # # # .
-            . . . . .
+            . . # . .
+            . . # . .
+            . . # . .
             `).showImage(0)
-        cakLandServos.turnServo(cakLandServos.ServoPin.P0, 0)
+        cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.HALF_DOWN)
         forwardQ = 1
+        basic.pause(500)
     }
 })
-basic.forever(function () {
-    cakLandMotor.turnLeft(63)
+loops.everyInterval(750, function () {
+	
 })
