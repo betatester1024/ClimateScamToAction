2fe88b954955c86cf7d0fff40ba104d3364b5655
John Doe <betatester1025@protonmail.com>
2023-05-09 13:48:17 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ input.onButtonPressed(Button.A, function () {
     cakLandServos.resetServos(cakLandServos.ServoPin.P0)
 })
 let forwardQ = 0
+let tooWetQ = 0
 images.createBigImage(`
     . . . . # . . . . .
     . . . # . . . . # .
@@ -9,44 +10,72 @@ images.createBigImage(`
     . # . . . . . . # .
     . . . . . . . . . .
     `).scrollImage(1, 200)
+let imgForward = images.createImage(`
+    # . # . .
+    # . # # .
+    # . # # #
+    # . # # .
+    # . # . .
+    `)
+let imgPause = images.createImage(`
+    . . . . .
+    . # . # .
+    . # . # .
+    . # . # .
+    . . . . .
+    `)
+let imgDown = images.createImage(`
+    . . # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    . . # . .
+    `)
+let imgWet = images.createImage(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
 cakLandServos.resetServos(cakLandServos.ServoPin.P0)
 cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.UP)
 cakLandPump.pumpStart(cakLand.BoardSide.RIGHT, 34)
 basic.forever(function () {
-	
+    if (cakLandSoil.ifMoisture(cakLandSoil.SoilPin.P0, cakLandSoil.Mlevel.VERY_WET)) {
+        tooWetQ = 1
+        imgWet.showImage(0)
+    } else {
+        tooWetQ = 0
+    }
+    if (cakLandTouch.getTouch(cakLandTouch.TouchPin.P1)) {
+        imgDown.showImage(0)
+        cakLandMotor.turnRight(50)
+        basic.pause(500)
+        cakLandMotor.turnRight(0)
+    }
 })
 loops.everyInterval(2300, function () {
-    images.createImage(`
-        . . . . .
-        . . . # .
-        # # # # #
-        . . . # .
-        . . . . .
-        `).showImage(0)
-    cakLandMotor.turnLeft(63)
-    basic.pause(500)
-    if (forwardQ == 1) {
-        cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.MIDDLE)
-        images.createImage(`
-            # . # . .
-            # . # # .
-            # . # # #
-            # . # # .
-            # . # . .
-            `).showImage(0)
-        basic.pause(500)
-        forwardQ = 0
+    if (tooWetQ == 0) {
+        imgForward.showImage(0)
+    }
+    if (tooWetQ > 0) {
+        cakLandMotor.turnLeft(20)
     } else {
-        cakLandMotor.stop()
-        images.createImage(`
-            . . # . .
-            . . # . .
-            . . # . .
-            . # # # .
-            . . # . .
-            `).showImage(0)
-        cakLandServos.setServoPosition(cakLandServos.ServoPin.P0, cakLandServos.Position.HALF_DOWN)
-        forwardQ = 1
+        cakLandMotor.turnLeft(63)
         basic.pause(500)
+        cakLandMotor.stop()
+        if (forwardQ == 1) {
+            imgPause.showImage(0)
+            basic.pause(500)
+            forwardQ = 0
+        } else {
+            imgDown.showImage(0)
+            basic.pause(300)
+            cakLandMotor.turnRight(50)
+            forwardQ = 1
+            basic.pause(200)
+            cakLandMotor.stop()
+        }
     }
 })
